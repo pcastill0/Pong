fb64d0125cdb08c1a23b1f1a0eeac14b3ba162ea
pcastill0 <pedro.castillo@enti.cat>
2023-12-11 11:18:26 +0100

    Primer commit

---
+++ Assets/Scripts/LimiteMovimiento.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimiteMovimiento : MonoBehaviour
+{
+    public float limiteXpositivo;
+    public float limiteXnegativo;
+
+    public float limiteYpositivo;
+    public float limiteYnegativo;
+
+    void Update()
+    {
+        if (limiteYpositivo < transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, limiteYpositivo, 0);
+        }
+
+        if (limiteYnegativo > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, limiteYnegativo, 0);
+        }
+
+        if (limiteXpositivo < transform.position.x)
+        {
+            transform.position = new Vector3(limiteXpositivo, transform.position.y, 0);
+        }
+
+        if (limiteXnegativo > transform.position.x)
+        {
+            transform.position = new Vector3(limiteXnegativo, transform.position.y, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PalaIA.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PalaIA : MonoBehaviour
+{
+    public GameObject pelota;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = 0;
+        if(transform.position.y > pelota.transform.position.y)
+        {
+            vertical = -1;
+        }
+
+        if (transform.position.y < pelota.transform.position.y)
+        {
+            vertical = 1;
+           
+        } 
+        transform.position += new Vector3(0, vertical, 0) * Time.deltaTime * speed;
+    }
+}
